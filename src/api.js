ea5f6d9a7f0bf57053c441a4c100ef52f7df8580
dharika9779-crypto <dharika9779@gmail.com>
2026-03-29 16:14:19 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = "https://homefood-backend-2.onrender.com";
-
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token')
   if (token) req.headers.Authorization = `Bearer ${token}`
